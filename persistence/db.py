e4cae02cf797cb80d87e054efb29f0c30759e0c6
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-07 00:28:56 -0700

    Eliminando caché

--- persistence/db.py
+++ persistence/db.py
@@ -3,8 +3,8 @@ from mysql.connector import Error
 
 def get_connection():
     return mysql.connector.connect(
-        host="",
-        user="",
-        password="",
-        database=""
+        host="ls-ef4300d410ebc1a8304030b638b03ed2da87f5c1.c90u2ss6wl6d.us-east-2.rds.amazonaws.com",
+        user="dbmasteruser",
+        password="SicnoLab2025",
+        database="CheemsTour"
     )
