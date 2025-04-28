aabf2d88dd891e56092b786bdacdfb5b0bdf771d
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-27 23:03:55 -0700

    Agregando endpoints para update y delete

--- app.py
+++ app.py
@@ -16,5 +16,17 @@ def save_trip():
     success = id is not None
     return jsonify(success), 201
 
+@app.route('/trips/<int:trip_id>', methods=['PUT'])
+def update_trip(trip_id):
+    data = request.get_json()
+    trip = Trip(name=data['name'], city=data['city'], country=data['country'])
+    success = Trip.update(trip_id, trip)
+    return jsonify(success), 200 if success else 404
+
+@app.route('/trips/<int:trip_id>', methods=['DELETE'])
+def delete_trip(trip_id):
+    success = Trip.delete(trip_id)
+    return jsonify(success), 200 if success else 404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
